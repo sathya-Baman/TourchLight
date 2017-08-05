81600b99f4415c9542d759c95ac0dd158ad01019
sathyabaman <sathyabaman@isaithuli.com>
2017-08-05 12:53:36 +0530

    Camera works on lollypop

--- app/src/main/java/lk/lankahomes/baman/tourchlight/TourchMain.java
+++ app/src/main/java/lk/lankahomes/baman/tourchlight/TourchMain.java
@@ -1,8 +1,15 @@
 package lk.lankahomes.baman.tourchlight;
 
+import android.Manifest;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,73 +23,110 @@ import android.content.pm.PackageManager;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class TourchMain extends Activity {
 
-    public static Camera cam = null;
+    public Camera cam = null;
+    public Context context;
+    static Camera mCam;
+    Boolean clickOn = false;
+    private static final int FLASHLIGHT_PERMISSIONS_REQUEST = 1;
+
+
+    public  ImageButton img_on;
+    public  ImageButton img_off;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tourch_main);
+        context= getApplicationContext();
 
-        final ImageButton img_on = (ImageButton) findViewById(R.id.im_btn_green_light);
-        final ImageButton img_off = (ImageButton) findViewById(R.id.im_btn_greendark);
-        img_on.setVisibility(View.GONE);
-        img_on.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+            Toast.makeText(getBaseContext(), "Your phone has a Flash", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getBaseContext(), "Your phone has NO Flash", Toast.LENGTH_SHORT).show();
+        }
+
+        checkCameraPermission();
+
+        img_on = (ImageButton) findViewById(R.id.im_btn_green_light);
+        img_off = (ImageButton) findViewById(R.id.im_btn_greendark);
+
+
+
+    }
+
+    public void lightsOn(View v){
+        clickOn = true;
+        processCamera();
+    }
+    public void lightsOff(View v){
+        clickOn = false;
+        processCamera();
+    }
+
+
+
+    private void processCamera(){
+        try{
+            if(clickOn == true) {
+                mCam = Camera.open();
+                Camera.Parameters p = mCam.getParameters();
+                p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                mCam.setParameters(p);
+                SurfaceTexture mPreviewTexture = new SurfaceTexture(0);
+                try {
+                    mCam.setPreviewTexture(mPreviewTexture);
+                } catch (IOException ex) {
+                    // Ignore
+                }
+                mCam.startPreview();
+
                 img_on.setVisibility(View.GONE);
                 img_off.setVisibility(View.VISIBLE);
-                offtourch();
 
-            }
-        });
-
-
-        img_off.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ontourch();
+            } else {
+                mCam.release();
                 img_on.setVisibility(View.VISIBLE);
                 img_off.setVisibility(View.GONE);
-
             }
-        });
-
-
-    }
-
-    private void ontourch(){
-//        try {
-//            if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-//                cam = Camera.open();
-//                Parameters p = cam.getParameters();
-//                p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-//                cam.setParameters(p);
-//                cam.startPreview();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            Toast.makeText(getBaseContext(), "Tourch Light ON", Toast.LENGTH_SHORT).show();
-//        }
-
-
-
-    }
-
-    private void offtourch(){
-        try {
-            if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-                cam.stopPreview();
-                cam.release();
-                cam = null;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(getBaseContext(), "Tourch Light OFF", Toast.LENGTH_SHORT).show();
+        } catch(Exception e) {
+            Log.e("Error", ""+e);
         }
     }
 
+
+
+
+
+
+
+
+
+
+
+    public void checkCameraPermission(){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                if (shouldShowRequestPermissionRationale( Manifest.permission.CAMERA)) { }
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, FLASHLIGHT_PERMISSIONS_REQUEST);
+            }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+             if (requestCode == FLASHLIGHT_PERMISSIONS_REQUEST) {
+                    if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "FlashLight permission granted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "FlashLight permission denied", Toast.LENGTH_SHORT).show();
+                    }
+        }  else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 
 
 }
